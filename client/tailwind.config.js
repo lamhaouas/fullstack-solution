6a096d6a2ed68b053ca1383c7923c164d46c8007
Si mohamed <m.lamhaouas@gmail.com>
2022-02-14 15:10:40 -0500

    change theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,4 +6,20 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        'mytheme': {                         
+           'primary' : '#fd2d01',         
+           'secondary' : '#ffd7d7',      
+           
+
+           'info' : '#2094f3',              
+           'success' : '#009485',           
+           'warning' : '#ff9900',           
+           'error' : '#ff5724',      
+        },
+      },
+    ],
+  },
 }
